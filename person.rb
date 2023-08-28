54190116e675c8ec76adab4b0eab0d1bebff7cd7
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-08-28 15:29:53 +0200

    Add student class

--- person.rb
+++ person.rb
@@ -1,25 +1,23 @@
 class Person
-    attr_reader :id, :name, :age
-    attr_writer :name, :age
-    def initialize(name = "unknown",age,parent_permission = true)
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
-    private
-    def of_age
-        if @age >= 18
-            return true
-        else
-            return false
-        end
-    end
-    def can_use_services
-        if @age >=18 || parent_permission
-            return true
-        end
-    end
+  attr_accessor :name, :age
+  attr_reader :id
 
-    
+  def initialize(age, name: 'unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age && @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 end
