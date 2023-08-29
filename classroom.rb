6c2a8f4a5804c3ab23f2d67596eb0ce4eba25972
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-08-29 16:21:21 +0200

    add relationship btn student and classroom classes

--- classroom.rb
+++ classroom.rb
@@ -1,6 +1,14 @@
 class Classroom
-    attr_accessor :label
-    def initialize(label)
-        @label = label
-    end
+  attr_accessor :label
+  attr_reader :students
+
+  def initialize(label)
+    @label = label
+    @student = []
+  end
+
+  def add_student(student)
+    students.push(student)
+    student.classroom = self
+  end
 end
