b2296beb63af79f2e66845cde63fd4d77ddf5304
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-08-30 16:34:16 +0200

    Add app class with its methods

---
+++ app.rb
@@ -0,0 +1,96 @@
+require_relative 'book'
+require_relative 'classroom'
+require_relative 'person'
+require_relative 'rental'
+require_relative 'student'
+require_relative 'teacher'
+class App
+  def initialize(_books, _people)
+    @books = []
+    @people = []
+  end
+
+  def create_person(person_code)
+    if person_code == 1
+      create_student
+    elsif person_code == 2
+      create_teacher
+    else
+      puts ' invalid input'
+      call(3)
+    end
+    puts 'Person created successfully'
+  end
+
+  def create_student
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Has parent permission? [Y/N]: '
+    has_permission = gets.chomp
+    permission_values = %w[n N]
+    student = Student.new(age, name, permission_values.includes?(has_permission))
+    people.push(student)
+  end
+
+  def create_teacher
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Specialization: '
+    specialization = gets.chomp
+    teacher = Teacher.new(age, specialization, name)
+    people.push(teacher)
+  end
+
+  def create_book(title, author)
+    # print "Title: "
+    # title = gets.chomp
+    # print "Author: "
+    # author = gets.chomp
+    book = Book.new(title, author)
+    books.push(book)
+    puts 'Book created successfully'
+  end
+
+  def list_all_books
+    puts books.inspect
+    books.each do |book|
+      puts "Title: \"#{book.title}\", Author: #{books.author}"
+    end
+  end
+
+  def list_all_people
+    people.each do |person|
+      type = person.instance_of?(Student) ? 'Student' : 'Teacher'
+      puts "[#{type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+  end
+
+  def create_rental
+    puts 'Select a book from the following list by number'
+    books.each_with_index do |book, index|
+      puts "#{index}) Title: \"#{book.title}\", Author: #{books.author}"
+    end
+    book_index = gets.chomp.to_i
+    puts 'Select a person from the following list by number (not id)'
+    people.each_with_index do |person, index|
+      puts "#{index}) [#{type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    person_index = gets.chomp.to_i
+    print 'Date: '
+    date = gets.chomp
+    Rental.new(date, @books[book_index], @people[person_index])
+    puts 'Rental created successfully'
+  end
+
+  def get_rental(id)
+    person = people.find { |person_id| person_id.id == id }
+    puts 'Rentals:'
+    person.rentals.each do |rental|
+      puts "Date: #{rental.date}, Book: \"{rental.book.title}\" by #{rental.book.author}"
+    end
+  end
+end
