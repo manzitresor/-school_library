e1951377268e0ab47976ad9ad4e342257e5a6e5c
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-08-30 23:35:59 +0200

    add main

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require_relative 'rental'
 require_relative 'student'
 require_relative 'teacher'
 class App
-  def initialize(_books, _people)
+  def initialize
     @books = []
     @people = []
   end
@@ -30,8 +30,8 @@ class App
     print 'Has parent permission? [Y/N]: '
     has_permission = gets.chomp
     permission_values = %w[n N]
-    student = Student.new(age, name, permission_values.includes?(has_permission))
-    people.push(student)
+    student = Student.new(age,permission_values.include?(has_permission),name)
+    @people.push(student)
   end
 
   def create_teacher
@@ -41,29 +41,24 @@ class App
     name = gets.chomp
     print 'Specialization: '
     specialization = gets.chomp
-    teacher = Teacher.new(age, specialization, name)
-    people.push(teacher)
+    teacher = Teacher.new(age, name, specialization,)
+    @people.push(teacher)
   end
 
   def create_book(title, author)
-    # print "Title: "
-    # title = gets.chomp
-    # print "Author: "
-    # author = gets.chomp
     book = Book.new(title, author)
-    books.push(book)
+    @books.push(book)
     puts 'Book created successfully'
   end
 
   def list_all_books
-    puts books.inspect
-    books.each do |book|
-      puts "Title: \"#{book.title}\", Author: #{books.author}"
+    @books.each do |book|
+      puts "Title: \"#{book.title}\", Author: #{book.author}"
     end
   end
 
   def list_all_people
-    people.each do |person|
+    @people.each do |person|
       type = person.instance_of?(Student) ? 'Student' : 'Teacher'
       puts "[#{type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
@@ -71,12 +66,13 @@ class App
 
   def create_rental
     puts 'Select a book from the following list by number'
-    books.each_with_index do |book, index|
-      puts "#{index}) Title: \"#{book.title}\", Author: #{books.author}"
+    @books.each_with_index do |book, index|
+      puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
     end
     book_index = gets.chomp.to_i
     puts 'Select a person from the following list by number (not id)'
-    people.each_with_index do |person, index|
+    @people.each_with_index do |person, index|
+      type = person.instance_of?(Student) ? 'Student' : 'Teacher'
       puts "#{index}) [#{type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
     person_index = gets.chomp.to_i
@@ -90,7 +86,7 @@ class App
     person = people.find { |person_id| person_id.id == id }
     puts 'Rentals:'
     person.rentals.each do |rental|
-      puts "Date: #{rental.date}, Book: \"{rental.book.title}\" by #{rental.book.author}"
+      puts "Date: #{rental.date}, Book: \"#{rental.book.title}\" by #{rental.book.author}"
     end
   end
 end
--- person.rb
+++ person.rb
@@ -6,11 +6,11 @@ class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id, :rentals
 
-  def initialize(age, name = 'unknown', parent_permission: true)
+  def initialize(age, name = 'unknown', parent_permission)
     super()
     @id = Random.rand(1..1000)
-    @name = name
     @age = age
+    @name = name
     @parent_permission = parent_permission
     @rentals = []
   end
@@ -35,10 +35,3 @@ class Person < Nameable
     rentals.push(rental)
   end
 end
-
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
