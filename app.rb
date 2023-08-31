be539b4533e057c1f6d8c9a968326e52b20c847c
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-08-31 11:09:51 +0200

    add changes

--- app.rb
+++ app.rb
@@ -10,48 +10,6 @@ class App
     @people = []
   end
 
-  def run
-    puts 'Welcome to School Library App!'
-    list_options
-  end
-
-  def list_options
-    loop do
-      puts 'Please choose an option by entering a number:'
-      puts '1 - List all books'
-      puts '2 - List all people'
-      puts '3 - Create a person'
-      puts '4 - Create a book'
-      puts '5 - Create a rental'
-      puts '6 - List all rentals for a given person id'
-      puts '7 - Exit'
-
-      number = gets.chomp.to_i
-      case number
-      when 1..6 then execute_option(number)
-      when 7 then puts 'Thank you for using this app!'
-      end
-      break if number == 7
-    end
-  end
-
-  def execute_option(response)
-    case response
-    when 1
-      list_all_books
-    when 2
-      list_all_people
-    when 3
-      create_person_option
-    when 4
-      create_book_option
-    when 5
-      create_rental
-    when 6
-      rental_option
-    end
-  end
-
   def create_person(person_code)
     if person_code == 1
       create_student
